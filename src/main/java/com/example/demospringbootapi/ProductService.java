1ab6e90ca8eec8baf7d08a5ec145fbce25abe374
Korivi Sukanya <korivisukanya02@gmail.com>
2026-08-17 19:18:57 +0530

    Implement product service

--- src/main/java/com/example/demospringbootapi/ProductService.java
+++ src/main/java/com/example/demospringbootapi/ProductService.java
@@ -1,10 +1,26 @@
 package com.example.demospringbootapi;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
   public Product createProduct(Product product){
      return product;
 
      }
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
   }
 
