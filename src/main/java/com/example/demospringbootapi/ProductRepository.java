e67a3e655733d8f2004ac8ae73bec14c68f949ed
Korivi Sukanya <korivisukanya02@gmail.com>
2026-08-17 19:18:20 +0530

    Update Product repository

--- src/main/java/com/example/demospringbootapi/ProductRepository.java
+++ src/main/java/com/example/demospringbootapi/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.demospringbootapi;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository
+        extends JpaRepository<Product, Integer> {
 }
